128c123c4ab8e9ce243f125909b92f3cdeac4df4
Anna <annsperkach@gmail.com>
2023-03-25 17:51:03 +0200

    animation idle+walk to the character

--- Assets/Character/PlayerMovement.cs
+++ Assets/Character/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public CharacterController2D controller;
+public Animator animator;
+
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
@@ -14,6 +16,8 @@ public class PlayerMovement : MonoBehaviour {
     void Update()
     {
      horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+     animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
      if (Input.GetButtonDown("Jump"))
      {
