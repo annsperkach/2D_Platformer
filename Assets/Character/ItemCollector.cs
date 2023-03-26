f7aa8e57d2249195d7f410c4c30f16f0d5d7d6c2
Anna <annsperkach@gmail.com>
2023-03-26 18:15:54 +0300

    ItemCollector, player collects the beer

---
+++ Assets/Character/ItemCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int beer = 0;
+
+    [SerializeField]private Text beerText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            Destroy(collision.gameObject);
+                beer++;
+                if (beerText != null) {
+                beerText.text = "Beer amount: " + beer;
+                }
+        }
+    }
+}
